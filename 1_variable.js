55fe692724507e58c6971b461f7d79fd0c0048d2
Suvadwip2004 <suvadwip14@gmail.com>
2025-04-06 18:09:28 +0000

    ok

---
+++ 1_variable.js
@@ -0,0 +1,7 @@
+const accountId  =  11425
+let accountEmaul  = "hello@gmail.com"
+var account_pass  = "gygfft"
+account_city  = "kolkata"
+// accountId = 2
+console.log(accountId);
+console.table([accountId, accountEmaul, account_pass, account_city])
---
+++ q.js
@@ -0,0 +1,5 @@
+let a = 25
+let b = 30
+let c
+c  = a + b
+console.log(c)
